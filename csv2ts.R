05ce7d3aef978f8d5527a013df7ea7766c230eb0
arthursmalliii <arthur.small@ventirisk.com>
2012-12-24 11:43:31 -0500

    Deprecating csv2ts function.
    Instead will read in CSV file in main program, create separate outcomes.df2ts function to convert outcomes data frame into time series object.

--- csv2ts.R
+++ csv2ts.R
@@ -4,8 +4,33 @@
 
 csv2ts <- function(csvFile){
      #  Perform transformations on user data:
-     #   - Convert text dates + times into date-times in the POSIXt class
-     #   - Replace initial_datetime with new column of forecast lead/lag times
-     #   - Add new column labels
-     #   - Reshape data frame into table by validTime, leadTime
+     NYC.temp.load <- read.table(csvFile, header = TRUE, sep = ",")
+     
+     #     Convert text dates into R's internal "Date" class
+     NYC.temp.load$Date <- as.Date(NYC.temp.load$Date,'%m/%d/%Y')
+     
+     #  Check for missing values in the load series
+     NYC.temp.load[is.na(NYC.temp.load$Load.MW), ]
+     
+     #  Perform ad-hoc plugs of missing values
+     NYC.temp.load$Load.MW[6727] <- (NYC.temp.load$Load.MW[6728]+NYC.temp.load$Load.MW[6728])/2
+     NYC.temp.load$Load.MW[6727]
+     NYC.temp.load$Load.MW[14992] <- (NYC.temp.load$Load.MW[14993]+NYC.temp.load$Load.MW[14991])/2
+     NYC.temp.load$Load.MW[14992]
+     
+     #  Check for impossibly low values in the load series
+     NYC.temp.load[NYC.temp.load$Load.MW < 1000, ]
+     
+     #  Check for missing values in the temperature series
+     NYC.temp.load[is.na(NYC.temp.load$Temp.Faren), ]
+     #  None!
+     
+     #  Convert NYC load series into a time series object
+     NYC.load.ts <- ts(NYC.temp.load$Load.MW, 
+                       # start     = 2005+1/12,
+                       frequency = 7*24)
+     NYC.load.ts
+     # NYC.temp.load$Date[1]
+     # plot(NYC.load.ts)
+     # NYC.load.ts[is.na(NYC.load.ts)]
 }
