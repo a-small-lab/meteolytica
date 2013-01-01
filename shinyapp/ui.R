45c306a11168ae300ec9796a4886acb7fd15a353
arthursmalliii <arthur.small@ventirisk.com>
2013-01-01 18:20:19 -0500

    Restructured ui page so that tabsets are top level.
    Use of bootstrapPage() layout had the effect of screwing up the margins. Spent some time trying to figure out a fix. Couldn't get it yet, but now I'm moving on to higher-value improvements.

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -20,74 +20,46 @@ electricity load for New York City from 2005--2008.",
 "This is a very early and unstable version of the system. If the program crashes, simply reload the browser to restart."
 )
 
+doc <- tags$html(
 
-shinyUI(pageWithSidebar(
-     
-     headerPanel("Meteolytica Demo"),
-  
-  # Sidebar with control to select the number of forecast periods to display
-     sidebarPanel(       
-        
-#         conditionalPanel(
-#            condition = "input.openTab == 'welcome' ",
-#            helpText("Click on the tabs, proceeding from left to right ",
-#                     "to see context-specific instructions.")
-#         ),
-#         
-# #         tabsetPanel(id='dist',
-# #                     tabPanel("Normal", value='norm', textInput("dist1","Xdist1", c("norm"))), 
-# #                     tabPanel("Uniform", value='unif', textInput("dist2","Xdist2", c("unif"))), 
-# #                     tabPanel("Log-normal", value='lnorm', textInput("dist3","Xdist3", c("lnorm"))), 
-# #                     tabPanel("Exponential", value='exp', textInput("dist4","Xdist4", c("exp"))) 
-# #         ), 
-# #         br(), 
-#         
-#        conditionalPanel(
-#             condition = "input.openTab == 'upload' ",
-#             fileInput("file", "Upload a time series file", multiple = FALSE, accept = NULL)
-#             ),
-#        
-#        conditionalPanel(
-#             condition = "input.openTab == 'view' ",
-#             helpText("Your data.")
-#             ),
-#      
-#        conditionalPanel(
-#             condition = "input.openTab == 'create' ",
-#             numericInput(inputId = "trainingPeriods",
-#                     label = "Number of weeks of data to use for training",
-#                     3),
-#             numericInput(inputId = "testingPeriods",
-#                     label = "Number of weeks of data to use for testing",
-#                     0)
-#             ),
-#        
-#        conditionalPanel(
-#             condition = "input.openTab == 'plot' ",
-#             sliderInput(inputId = "display_periods",
-#                         label = "Number of forecast weeks to display in plot:",
-#                         min = 0.2, max = 2, step = 0.2, value = 1)            
-#             ),
-#        
-#        conditionalPanel(
-#             condition = "input.openTab == 'performance' ",
-#             numericInput("numberOfObsToDisplay", 
-#                          "Number of observations to display in table:", 
-#                          10)            
-#             )
+  tags$head(
+    tags$title('My first page'),
+#    tags$style(tags$body(margin='100px'))
+    tags$style("body {
+            background-color: #CCC;
+        }
+               
+               #SiteBody 
+        {
+            width: 960px;
+            margin: 0 auto;
+            background-color: white;
+        }")
+  ),
+  tags$body(tags$div(id='SiteBody', class='container')
 
-       ),
-  
-   # Show a tabset that includes a plot, summary, and table view
-   # of the data set and associated forecast
-     
-  mainPanel(
-    tabsetPanel(id="openTab",
-                tabPanel("Welcome", value='welcome', welcomeMessage),
-                
-                tabPanel("[Placeholder]", value='placeholder',
-                         pageWithSidebar(
-                           
+  )
+)
+
+cat(as.character(doc))
+
+#<body leftmargin="0px" topmargin="0px" marginwidth="0px" marginheight="0px">,
+
+
+shinyUI(
+bootstrapPage(
+h1("Meteolytica Demo"),
+tabsetPanel(id="openTab",
+            tabPanel("Welcome", value='welcome',          
+                     pageWithSidebar(
+                       headerPanel("Welcome to Meteolytica"),
+                       sidebarPanel(),
+                       mainPanel(welcomeMessage)
+                       )
+                     ),
+
+            tabPanel("[Placeholder]", value='placeholder',
+                     pageWithSidebar(                           
                            headerPanel("Meteolytica Demo"),
                            
                            # Sidebar with control to select the number of forecast periods to display
@@ -147,45 +119,44 @@ shinyUI(pageWithSidebar(
                            
                          )),
                   
-                tabPanel("Your Project", value='project',
-                        tabsetPanel(id='projectTab',
-                                    tabPanel("Describe your project", value='describeProject'),
-                                    tabPanel("Test 3"),
-                                    tabPanel("Test 4")
-                                  )
-                        ),
+            tabPanel("Your Project", value='project',
+                    tabsetPanel(id='projectTab',
+                                tabPanel("Describe your project", value='describeProject'),
+                                tabPanel("Test 3"),
+                                tabPanel("Test 4")
+                                )
+                    ),
                   
-                tabPanel("Your Data", value='data',
-                        tabsetPanel(id="dataTab",
-                                    tabPanel("Upload your data", value='uploadData'),
-                                    tabPanel("View your data", value='viewData', 
-                                              verbatimTextOutput("outcomesSummary"),
-                                              tableOutput("tableOfUsersData")
-                                              )
-                                    )
-                        ),
+            tabPanel("Your Data", value='data',
+                    tabsetPanel(id="dataTab",
+                                tabPanel("Upload your data", value='uploadData'),
+                                tabPanel("View your data", value='viewData', 
+                                         verbatimTextOutput("outcomesSummary"),
+                                         tableOutput("tableOfUsersData")
+                                         )
+                                )
+                    ),
                 
-                tabPanel("Forecast Model Generator", value='model',
-                         bootstrapPage(
-                           tabsetPanel(id='modelTab',
-                                       tabPanel("Create forecasting model", value='create', 
-                                                plotOutput('decomposedTsPlot')
-                                                ),
-                                       tabPanel("View plot", value='plot', 
-                                                plotOutput("forecastPlot")
-                                                )
-                                       )
-                           )
-                         ), 
+            tabPanel("Forecast Model Generator", value='model',
+                     bootstrapPage(
+                       tabsetPanel(id='modelTab',
+                                   tabPanel("Create forecasting model", value='create', 
+                                            plotOutput('decomposedTsPlot')
+                                            ),
+                                   tabPanel("View plot", value='plot', 
+                                            plotOutput("forecastPlot")
+                                            )
+                                   )
+                       )
+                     ), 
 
-                tabPanel("Forecast Accuracy", value='performance', 
+             tabPanel("Forecast Accuracy", value='performance', 
                         tableOutput("accuracy")
                         )
 
-                  )  #  Close top-level tabsetPanel()
-            )        #  Close mainPanel()
-         )           #  Close pageWithSidebar()
-   )                 #  Close ShinyUI()
+              )  #  Close top-level tabsetPanel()
+      )          #  Close bootstrapPage()
+   )             #  Close ShinyUI()
 
 
 ########### DEPRECATED CODE (you may ignore everything below here) #############
